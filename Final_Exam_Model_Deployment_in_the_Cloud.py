24d04f49699f91eeb8bdfbb3baa2110f93b321d4
EthyCode <0silent0playz0@gmail.com>
2024-05-18 15:32:50 +0800

    Update Final_Exam_Model_Deployment_in_the_Cloud.py

--- Final_Exam_Model_Deployment_in_the_Cloud.py
+++ Final_Exam_Model_Deployment_in_the_Cloud.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('plant_classifier.hdf5')
+  model=tf.keras.models.load_model('earthquake_magnitude_classifier')
   return model
 model=load_model()
 st.write("""
