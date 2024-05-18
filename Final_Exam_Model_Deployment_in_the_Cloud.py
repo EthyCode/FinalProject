27b7e18eaa62854d0efcf3aa6870b56857ed2e17
EthyCode <0silent0playz0@gmail.com>
2024-05-18 20:34:33 +0800

    Update Final_Exam_Model_Deployment_in_the_Cloud.py

--- Final_Exam_Model_Deployment_in_the_Cloud.py
+++ Final_Exam_Model_Deployment_in_the_Cloud.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('earthquake_magnitude_classifier.h5')
+  model=tf.keras.models.load_model('Cifar10_Models.h5')
   return model
 model=load_model()
 st.write("""# CIFAR10 Detection System""")
